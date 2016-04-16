4e8fe4f99b3012a88948837994e615812f97a1e8
deoxxa <deoxxa@fknsrs.biz>
2016-04-16 22:17:41 +1000

    test all the things

--- urlqp_test.go
+++ urlqp_test.go
@@ -9,6 +9,10 @@ import (
 func TestParse(t *testing.T) {
 	a := assert.New(t)
 
+	if v, err := Parse(``); a.NoError(err) {
+		a.Nil(v)
+	}
+
 	if v, err := Parse(`a=b&c=d&e=f`); a.NoError(err) {
 		a.Equal(Values{
 			Pair{"a", "b"},
@@ -58,9 +62,19 @@ func TestParse(t *testing.T) {
 			Pair{",", "x"},
 		}, v)
 	}
+
+	{
+		_, err := Parse(`%`)
+		a.Error(err)
+	}
+
+	{
+		_, err := Parse(`a=%`)
+		a.Error(err)
+	}
 }
 
-func TestSerialize(t *testing.T) {
+func TestString(t *testing.T) {
 	a := assert.New(t)
 
 	a.Equal(`a=b&c=d`, (Values{
@@ -82,4 +96,49 @@ func TestSerialize(t *testing.T) {
 		Pair{"a", ","},
 		Pair{",", "a"},
 	}).String())
+}
+
+func TestGet(t *testing.T) {
+	a := assert.New(t)
+
+	v, err := Parse(`a=b&c=d&e=f&e=g`)
+	a.NoError(err)
+
+	a.Equal("b", v.Get("a"))
+	a.Equal("d", v.Get("c"))
+	a.Equal("f", v.Get("e"))
+	a.Empty(v.Get("x"))
+}
+
+func TestAll(t *testing.T) {
+	a := assert.New(t)
+
+	v, err := Parse(`a=b&c=d&e=f&e=g`)
+	a.NoError(err)
+
+	a.Equal([]string{"b"}, v.All("a"))
+	a.Equal([]string{"d"}, v.All("c"))
+	a.Equal([]string{"f", "g"}, v.All("e"))
+	a.Nil(v.All("x"))
+}
+
+func TestFilter(t *testing.T) {
+	a := assert.New(t)
+
+	v, err := Parse(`a=b&c=d&e=f&e=g`)
+	a.NoError(err)
+
+	a.Equal(Values{
+		Pair{"a", "b"},
+	}, v.Filter("a"))
+
+	a.Equal(Values{
+		Pair{"a", "b"},
+		Pair{"c", "d"},
+	}, v.Filter("a", "c"))
+
+	a.Equal(Values{
+		Pair{"e", "f"},
+		Pair{"e", "g"},
+	}, v.Filter("e"))
 }
